5b78dd3686dd9ce7578c315155e0cda04902cdbf
Douglas Chan <douglaschan32167@berkeley.edu>
2013-05-01 22:27:21 -0700

    cat now catches mouse
    fixed a bug where the cat would continue to move towards the statue
    after seeing the mouse and being at the same radius

--- animals.cpp
+++ animals.cpp
@@ -42,15 +42,27 @@ Cat::Cat(string s, Position p)
 
 bool Cat::Chase() {
 	if (myPos.Sees(myTarget->Pos())) {
+		if (myTarget->Pos().getRadius() == myPos.getRadius()) {
+			Position temp = myPos;
+			temp.moveDistance(2.0);
+			if (myTarget->Pos().IsBetween(myPos, temp)) {
+				cout << "cat has caught the mouse" << endl;
+				return true;
+			}
+		}
+		cout << "cat sees target" << endl;
 		myPos.IncrementPosition(1.0, 0.0);
+		return false;
 	} else {
 		Position temp = myPos;
+		cout << "cat moves" << endl;
 		temp.moveDistance(2.0);
 		if (myTarget->Pos().IsBetween(myPos, temp)) {
 			cout << "cat caught mouse" << endl;
 			return true;
 		} else {
 			myPos.moveDistance(2.0);
+			return false;
 		}
 	}
 }
