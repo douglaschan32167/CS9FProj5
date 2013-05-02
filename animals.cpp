ce7e7d904d0b20169a6dc184ea0f43c3a2998085
Douglas Chan <douglaschan32167@berkeley.edu>
2013-05-02 14:53:37 -0700

    math now works correctly
    fixed a bug where the cat would catch the mouse at incorrect times and
    all the animals would move incorrect amounts.

--- animals.cpp
+++ animals.cpp
@@ -14,8 +14,8 @@ Mouse::Mouse(string s, Position p)
 
 bool Mouse::Chase() {
 	float rad = myPos.getRadius();
-	myPos.moveDistance(1.25);
-	cout << "mouse moves"<< endl;
+	myPos.moveDistance(1.0);
+	cout << "mouse moves to radius"<< myPos.getRadius() << "and angle" << myPos.getAngleInRadians() << endl;
 	return false;
 }
 
@@ -29,8 +29,9 @@ bool Person::Chase() {
 		cout << "person sees target" << endl;
 		return false;
 	} else {
-		cout << "person moves" << endl;
-		myPos.moveDistance(2.0);
+		
+		myPos.moveDistance(-2.0);
+		cout << "person moves to radius" << myPos.getRadius() << "and angle" << myPos.getAngleInRadians() << endl;
 		return false;
 	}
 }
@@ -42,26 +43,32 @@ Cat::Cat(string s, Position p)
 
 bool Cat::Chase() {
 	if (myPos.Sees(myTarget->Pos())) {
+		cout << "cat sees target" << endl;
 		if (myTarget->Pos().getRadius() == myPos.getRadius()) {
 			Position temp = myPos;
-			temp.moveDistance(2.0);
+			temp.moveDistance(1.25);
 			if (myTarget->Pos().IsBetween(myPos, temp)) {
 				cout << "cat has caught the mouse" << endl;
 				return true;
+			} else {
+				myPos.moveDistance(1.25);
+				cout << "cat has moved to radius" <<myPos.getRadius() << "and angle" << myPos.getAngleInRadians() << endl;
+				return false;
 			}
 		}
-		cout << "cat sees target" << endl;
-		myPos.IncrementPosition(1.0, 0.0);
+
+		myPos.IncrementPosition(-1.0, 0.0);
 		return false;
 	} else {
 		Position temp = myPos;
-		cout << "cat moves" << endl;
-		temp.moveDistance(2.0);
-		if (myTarget->Pos().IsBetween(myPos, temp)) {
+
+		temp.moveDistance(1.25);
+		if (myTarget->Pos().IsBetween(myPos, temp) && myPos.getRadius() == myTarget->Pos().getRadius()) {
 			cout << "cat caught mouse" << endl;
 			return true;
 		} else {
-			myPos.moveDistance(2.0);
+			myPos.moveDistance(1.25);
+			cout << "cat has moved to radius" <<myPos.getRadius() << "and angle" << myPos.getAngleInRadians() << endl;
 			return false;
 		}
 	}
